35df61aa6e0fa008dfddd0ce6f95ac80ee1b50b2
john3corrigan <john.corrigan@onshoreoutsourcing.com>
2018-11-01 16:02:32 -0400

    commit for 11_1_2018

--- BookLibraryV2/Models/UserAccount.cs
+++ BookLibraryV2/Models/UserAccount.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookLibraryV2.Models
 {
     public class UserAccount
     {
         public int UserID { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [RegularExpression("[A-Z]*")]
         public string Password { get; set; }
         public UserRole UserRole { get; set; }
 
